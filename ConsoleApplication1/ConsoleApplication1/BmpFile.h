3ed8770d344308228e7b5fd77b3140b50eb46b74
Simpro_the_Young <scm1400@gmail.com>
2021-02-25 15:15:03 +0900

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/BmpFile.h
@@ -0,0 +1,14 @@
+
+#pragma once
+#include <vector>
+#include "EpfFile.h"
+
+struct BmpData
+{
+	int m_width;
+	int m_height;
+	std::vector<Pixel> m_data;
+};
+
+
+int bmpextract(std::string fileName, BmpData* out);
